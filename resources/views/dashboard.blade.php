f94adbd50d5234158b6f9a146b8703d83bb4dc76
Christer Miroy <chrstrzm.mry@gmail.com>
2023-03-02 15:35:00 +0800

    final version of sidebar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             <!-- Display logged in user -->
-            Hello <b>{{ Auth::user() -> name }}</b>
+            Hello, <b>{{ Auth::user() -> name }}</b>!
         </h2>
     </x-slot>
 
